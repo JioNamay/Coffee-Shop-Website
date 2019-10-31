d31c4d5f506149d207839735eaaffb4bf2fce1a4
Raphael Namay <raphael.namay@gmail.com>
2019-11-01 01:14:49 +1300

    Added export statement

--- routes/products.js
+++ routes/products.js
@@ -12,31 +12,35 @@ pool = new Pool({
 });
 
 // GET all products
-router.get("/", (req, res) => {
-    const client = await pool.connect();
-    const userQuery = "SELECT * FROM products";
-    const result = await client.query(userQuery);
-    client.release();
-    res.status(200).json(result.rows);
+router.get("/", async (req, res) => {
+  const client = await pool.connect();
+  const userQuery = "SELECT * FROM products";
+  const result = await client.query(userQuery);
+  client.release();
+  res.status(200).json(result.rows);
 });
 
 // GET a specific product
-router.get("/:id", (req, res) => {
-    // if the id is not a number, send an error
-    const productId = parseInt(req.params.id);
-    if (Number.isNaN(productId)) return res.status(400).send("The ID must be a number.");
+router.get("/:id", async (req, res) => {
+  // if the id is not a number, send an error
+  const productId = parseInt(req.params.id);
+  if (Number.isNaN(productId))
+    return res.status(400).send("The ID must be a number.");
 
-    const client = await pool.connect();
-    const existsQuery = `SELECT EXISTS(SELECT id FROM products WHERE id = '${productId}')`;
-    const existsResult = await client.query(existsQuery);
-    
-    // if the task does not exist, send an error
-    const taskExists = existsResult.rows[0].exists;
-    if (!taskExists) return res.status(404).send("The task with the given ID was not found.");
+  const client = await pool.connect();
+  const existsQuery = `SELECT EXISTS(SELECT id FROM products WHERE id = '${productId}')`;
+  const existsResult = await client.query(existsQuery);
 
-    // retrieve the task from the database
-    const retrieveQuery = `SELECT * FROM products WHERE id = '${productId}'`;
-    const retrieveResult = await client.query(retrieveQuery);
-    client.release();
-    res.status(200).json(retrieveResult.rows[0]); // send the task
+  // if the task does not exist, send an error
+  const taskExists = existsResult.rows[0].exists;
+  if (!taskExists)
+    return res.status(404).send("The task with the given ID was not found.");
+
+  // retrieve the task from the database
+  const retrieveQuery = `SELECT * FROM products WHERE id = '${productId}'`;
+  const retrieveResult = await client.query(retrieveQuery);
+  client.release();
+  res.status(200).json(retrieveResult.rows[0]); // send the task
 });
+
+module.exports = router;

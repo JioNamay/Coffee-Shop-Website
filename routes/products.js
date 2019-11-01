6f7109ba4542682260b6b575201c390c44779931
Raphael Namay <raphael.namay@gmail.com>
2019-11-02 05:50:32 +1300

    Added try/catch blocks

--- routes/products.js
+++ routes/products.js
@@ -13,34 +13,42 @@ pool = new Pool({
 
 // GET all products
 router.get("/", async (req, res) => {
-  const client = await pool.connect();
-  const userQuery = "SELECT * FROM products";
-  const result = await client.query(userQuery);
-  client.release();
-  res.status(200).json(result.rows);
+  try {
+    const client = await pool.connect();
+    const userQuery = "SELECT * FROM products";
+    const result = await client.query(userQuery);
+    client.release();
+    res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(500).send("Something went wrong.");
+  }
 });
 
 // GET a specific product
 router.get("/:id", async (req, res) => {
-  // if the id is not a number, send an error
-  const productId = parseInt(req.params.id);
-  if (Number.isNaN(productId))
-    return res.status(400).send("The ID must be a number.");
+  try {
+    // if the id is not a number, send an error
+    const productId = parseInt(req.params.id);
+    if (Number.isNaN(productId))
+      return res.status(400).send("The ID must be a number.");
 
-  const client = await pool.connect();
-  const existsQuery = `SELECT EXISTS(SELECT id FROM products WHERE id = '${productId}')`;
-  const existsResult = await client.query(existsQuery);
+    const client = await pool.connect();
+    const existsQuery = `SELECT EXISTS(SELECT id FROM products WHERE id = '${productId}')`;
+    const existsResult = await client.query(existsQuery);
 
-  // if the task does not exist, send an error
-  const taskExists = existsResult.rows[0].exists;
-  if (!taskExists)
-    return res.status(404).send("The task with the given ID was not found.");
+    // if the task does not exist, send an error
+    const taskExists = existsResult.rows[0].exists;
+    if (!taskExists)
+      return res.status(404).send("The task with the given ID was not found.");
 
-  // retrieve the task from the database
-  const retrieveQuery = `SELECT * FROM products WHERE id = '${productId}'`;
-  const retrieveResult = await client.query(retrieveQuery);
-  client.release();
-  res.status(200).json(retrieveResult.rows[0]); // send the task
+    // retrieve the task from the database
+    const retrieveQuery = `SELECT * FROM products WHERE id = '${productId}'`;
+    const retrieveResult = await client.query(retrieveQuery);
+    client.release();
+    res.status(200).json(retrieveResult.rows[0]); // send the task
+  } catch (error) {
+    res.status(500).send("Something went wrong.");
+  }
 });
 
 module.exports = router;

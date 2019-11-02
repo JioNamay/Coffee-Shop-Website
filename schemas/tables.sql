50950b7099311fc687a5453ef5ccb319d9b982bf
Raphael Namay <raphael.namay@gmail.com>
2019-11-03 01:00:42 +1300

    Commented out a few statements to test database

--- schemas/tables.sql
+++ schemas/tables.sql
@@ -46,17 +46,17 @@ VALUES ('d73a0001-3dea-4aa7-aca6-f9cdd8ec89af', 'Bob', 'Smith', 'test@gmail.com'
 INSERT INTO items (name, description, price, image)
 VALUES ('Espresso', 'Espresso is generally thicker than coffee brewed by other methods, has a higher concentration of suspended and dissolved solids, and has a foam with a creamy consistency', 4.35, 'https://miro.medium.com/max/1200/1*4FzJWow3qJOV_O-3iKgBOw.jpeg');
 
-INSERT INTO items (name, description, price, image)
-VALUES ('Short Macchiato', 'An espresso coffee drink with a small amount of milk, usually foamed. In Italian, macchiato means "stained" or "spotted" so the literal translation of caffè macchiato is stained or marked coffee.', 4.70, 'https://coffeemakered.com/wp-content/uploads/2018/08/espresso-macchiato.jpg');
+-- INSERT INTO items (name, description, price, image)
+-- VALUES ('Short Macchiato', 'An espresso coffee drink with a small amount of milk, usually foamed. In Italian, macchiato means "stained" or "spotted" so the literal translation of caffè macchiato is stained or marked coffee.', 4.70, 'https://coffeemakered.com/wp-content/uploads/2018/08/espresso-macchiato.jpg');
 
-INSERT INTO items (name, description, price, image)
-VALUES ('Creamy Iced Coffee', 'The perfect balance of milk, coffee, coldness, and whatever else', 5.00, 'https://www.joyfulhealthyeats.com/wp-content/uploads/2018/04/Sweet-Cream-Iced-Coffee-web-9.jpg');
+-- INSERT INTO items (name, description, price, image)
+-- VALUES ('Creamy Iced Coffee', 'The perfect balance of milk, coffee, coldness, and whatever else', 5.00, 'https://www.joyfulhealthyeats.com/wp-content/uploads/2018/04/Sweet-Cream-Iced-Coffee-web-9.jpg');
 
-INSERT INTO items (name, description, price, image)
-VALUES ('Mocha', 'The best mocha in Victoria University', 4.00, 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/03/mocha-001.jpg?itok=pX2cX4i_');
+-- INSERT INTO items (name, description, price, image)
+-- VALUES ('Mocha', 'The best mocha in Victoria University', 4.00, 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/03/mocha-001.jpg?itok=pX2cX4i_');
 
-INSERT INTO items (name, description, price, image)
-VALUES ('Long Black', 'Coffee Something Coffee Something Coffee', 3.50, 'https://whitehorsecoffee.com.au/wp-content/uploads/2016/09/unnamed-6-1170x780.jpg');
+-- INSERT INTO items (name, description, price, image)
+-- VALUES ('Long Black', 'Coffee Something Coffee Something Coffee', 3.50, 'https://whitehorsecoffee.com.au/wp-content/uploads/2016/09/unnamed-6-1170x780.jpg');
 
-INSERT INTO items (name, description, price, image)
-VALUES ('Iced Macchiato', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 3.50, 'https://tastykitchen.com/recipes/wp-content/uploads/sites/2/2010/05/icedcaramelmacchiato.jpg');
+-- INSERT INTO items (name, description, price, image)
+-- VALUES ('Iced Macchiato', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 3.50, 'https://tastykitchen.com/recipes/wp-content/uploads/sites/2/2010/05/icedcaramelmacchiato.jpg');

277369150aff91a6a769938dd3d808350d957224
Duane Alcala <alcaladuan@ecs.vuw.ac.nz>
2019-11-02 22:36:16 +1300

    Added better functionality to signup and login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,164 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import * as EmailValidator from 'email-validator';
 
-const Login = () => {
+import { loginAction } from "../store/actions/userActions";
+import { tokenLoginAction } from "../store/actions/userActions";
+
+const Login = (props) => {
+  let {
+    user,
+    loginAction,
+    tokenLoginAction,
+  } = props;
+
+  useEffect(() => {
+    const tokenAvailable = localStorage.getItem('token');
+    if (tokenAvailable !== null) {
+      // Attempt to login with token
+      tokenLoginAction(tokenAvailable)
+        .then(() => {
+          props.history.push('/browse');
+        })
+        .catch((error) => {
+          // Invalid token, do nothing
+        });
+    }
+  }, []);
+
+  if (user !== null && user.token !== null) {
+    localStorage.setItem('token', user.token);
+  }
+
+  const [loginError, setLoginError] = useState({
+    errorMessage: '',
+    errorOccured: false,
+    showError: false
+  });
+
+  const [loginInfo, setLoginInfo] = useState({
+    email: '',
+    emailValid: false,
+    emailTouched: false,
+    password: '',
+    passwordValid: false,
+    passwordTouched: false
+  });
+
+  const displayError = () => {
+    setTimeout(() => {
+      setLoginError({
+        ...loginError,
+        showError: false
+      })
+    }, 5000);
+  };
+
+  const setEmail = (e) => {
+    const email = e.target.value;
+    const validEmail = EmailValidator.validate(email);
+    setLoginInfo({
+      ...loginInfo,
+      [e.target.name]: email,
+      emailValid: validEmail
+    })
+  };
+
+  const setPassword = (e) => {
+    const password = e.target.value;
+    const validPassword = password !== '';
+    setLoginInfo({
+      ...loginInfo,
+      [e.target.name]: e.target.value,
+      passwordValid: validPassword
+    })
+  };
+
+  const onBlur = (e) => {
+    const blurredField = e.target.name + 'Touched';
+    setLoginInfo({
+      ...loginInfo,
+      [blurredField]: true
+    })
+  };
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    try {
+      // Log user in and redirect
+      await loginAction(loginInfo.email, loginInfo.password);
+      props.history.push('/browse');
+    } catch (error) {
+      // Show alert on error
+      setLoginError({
+        ...loginError,
+        errorMessage: error.message,
+        showError: true
+      });
+      displayError();
+    }
+  };
+
   return (
-    <div>
-      <form action="/action_page.php">
-        <div className="form-group">
-          <label htmlFor="email">Email address:</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="pwd">Password:</label>
-          <input type="password" className="form-control" id="pwd" />
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+    <div className="container">
+      <div className="login-page">
+        <h1 className="login-header">Login</h1>
+        {
+          loginError.showError &&
+          <div className="alert alert-danger" role="alert">
+            {loginError.errorMessage}
+          </div>
+        }
+        <form onSubmit={(e) => {onLogin(e)}}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              required
+              value={loginInfo.email}
+              onChange={e => {setEmail(e)}}
+              onBlur={e => {onBlur(e)}}
+            />
+            {
+              loginInfo.emailTouched && !loginInfo.emailValid &&
+              <small><p className="error-message">Enter a valid email</p></small>
+            }
+          </div>
+          <div className="form-group">
+            <label htmlFor="pwd">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              required
+              value={loginInfo.password}
+              onChange={e => {setPassword(e)}}
+              onBlur={e => {onBlur(e)}}
+            />
+            {
+              loginInfo.passwordTouched && !loginInfo.passwordValid &&
+              <small><p className="error-message">Password required</p></small>
+            }
+          </div>
+          <button
+            type="submit"
+            className="login-button"
+            disabled={
+              !loginInfo.emailValid || !loginInfo.passwordValid
+            }
+          >Login</button>
+        </form>
+      </div>
     </div>
   )
 };
 
-export default Login;
+const mapStateToProps = state => {
+  return {
+    user: state.user.user
+  }
+};
+
+export default connect(mapStateToProps, { loginAction, tokenLoginAction })(Login);
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,31 +1,205 @@
-import React, {Fragment, useState} from 'react';
+import React, { useState } from 'react';
 import PasswordStrengthBar from 'react-password-strength-bar';
+import { connect } from 'react-redux';
+import * as EmailValidator from 'email-validator';
 
-const Signup = () => {
-  const [password, changePassword] = useState('');
+import { signupAction } from "../store/actions/userActions";
 
-  const onChange = (e) => {
-    changePassword(e.target.value);
+const Signup = (props) => {
+  const [signupError, setSignupError] = useState({
+    errorMessage: '',
+    errorOccured: false,
+    showError: false
+  });
+
+  const [signupInfo, setSignupInfo] = useState({
+    firstName: '',
+    firstNameTouched: false,
+    firstNameValid: false,
+    lastName: '',
+    lastNameValid: false,
+    lastNameTouched: false,
+    email: '',
+    emailValid: false,
+    emailTouched: false,
+    password: '',
+    passwordValid: false,
+    passwordTouched: false,
+    confirmPassword: '',
+    confirmPasswordTouched: false,
+    passwordMatches: false
+  });
+
+  const displayError = () => {
+    setTimeout(() => {
+      setSignupError({
+        ...signupError,
+        showError: false
+      })
+    }, 5000);
+  };
+
+  const setInput = (e) => {
+    const name = e.target.value;
+    let validFirstName = name !== '';
+    let targetValid = e.target.name + 'Valid';
+    setSignupInfo({
+      ...signupInfo,
+      [e.target.name]: e.target.value,
+      [targetValid]: validFirstName
+    })
+  };
+
+  const setEmail = (e) => {
+    const email = e.target.value;
+    const validEmail = EmailValidator.validate(email);
+    setSignupInfo({
+      ...signupInfo,
+      [e.target.name]: email,
+      emailValid: validEmail
+    });
+  };
+
+  const setConfirmPassword = (e) => {
+    const confirmPass = e.target.value;
+    let passwordMatch = confirmPass === signupInfo.password;
+    setSignupInfo({
+      ...signupInfo,
+      confirmPassword: confirmPass,
+      passwordMatches: passwordMatch
+    });
+  };
+
+  const onBlur = (e) => {
+    const blurredField = e.target.name + 'Touched';
+    setSignupInfo({
+      ...signupInfo,
+      [blurredField]: true
+    })
+  };
+
+  const onSignup = async (e) => {
+    e.preventDefault();
+    try {
+      // Sign the user up and then redirect them
+      await props.signupAction(signupInfo.firstName, signupInfo.lastName, signupInfo.email, signupInfo.password);
+      props.history.push('/login');
+    } catch (error) {
+      // Show alert on error
+      setSignupError({
+        ...signupError,
+        errorMessage: error.message,
+        showError: true
+      });
+      displayError();
+    }
   };
 
   return (
-    <Fragment>
-      <div>
-        <form>
+    <div className="container">
+      <div className="signup-page">
+        <h1 className="signup-header">Sign Up</h1>
+        {
+          signupError.showError &&
+          <div className="alert alert-danger" role="alert">
+            {signupError.errorMessage}
+          </div>
+        }
+        <form onSubmit={(e) => {onSignup(e)}}>
+          <div className="form-group">
+            <label>First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name='firstName'
+              required
+              value={signupInfo.firstName}
+              onChange={e => setInput(e)}
+              onBlur={(e) => {onBlur(e)}}
+            />
+            {
+              signupInfo.firstNameTouched && !signupInfo.firstNameValid &&
+              <small><p className="error-message">First name must not be empty</p></small>
+            }
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name='lastName'
+              required
+              value={signupInfo.lastName}
+              onChange={e => setInput(e)}
+              onBlur={(e) => {onBlur(e)}}
+            />
+            {
+              signupInfo.lastNameTouched && !signupInfo.lastNameValid &&
+              <small><p className="error-message">Last name must not be empty</p></small>
+            }
+          </div>
           <div className="form-group">
             <label>Email address</label>
-            <input type="email" className="form-control" />
+            <input
+              type="email"
+              className="form-control"
+              name='email'
+              required
+              value={signupInfo.email}
+              onChange={e => setEmail(e)}
+              onBlur={(e) => {onBlur(e)}}
+            />
+            {
+              signupInfo.emailTouched && !signupInfo.emailValid &&
+              <small><p className='error-message'>Enter a valid email</p></small>
+            }
           </div>
           <div className="form-group">
             <label>Password</label>
-            <input className="form-control" type="password" value={password} onChange={e => onChange(e)} />
-            <PasswordStrengthBar password={password} />
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              required
+              minLength={5}
+              value={signupInfo.password}
+              onChange={e => setInput(e)}
+              onBlur={(e) => {onBlur(e)}}
+            />
+            {
+              signupInfo.passwordTouched && !signupInfo.passwordValid &&
+              <small><p className='error-message'>Password must be at least 5 characters</p></small>
+            }
           </div>
-          <button type="submit" className="btn btn-primary">Sign Up</button>
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input
+              className="form-control"
+              type="password"
+              name="confirmPassword"
+              required
+              value={signupInfo.confirmPassword}
+              onChange={e => setConfirmPassword(e)}
+              onBlur={(e) => {onBlur(e)}}
+            />
+            {
+              signupInfo.confirmPasswordTouched && signupInfo.password !== '' && !signupInfo.passwordMatches &&
+              <small><p className='error-message'>Passwords do not match</p></small>
+            }
+          </div>
+          <PasswordStrengthBar password={signupInfo.password} />
+          <button
+            type="submit"
+            className="signup-button"
+            disabled={
+              !signupInfo.firstNameValid || !signupInfo.lastNameValid ||
+              !signupInfo.emailValid || !signupInfo.passwordValid || !signupInfo.passwordMatches
+            }
+          >Sign Up</button>
         </form>
       </div>
-    </Fragment>
+    </div>
   )
 };
 
-export default Signup;
+export default connect(null, { signupAction })(Signup);

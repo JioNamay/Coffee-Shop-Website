5bf7fd246167314a040ebdbcd3f1fa6c8d08fde5
Duane Alcala <alcaladuan@ecs.vuw.ac.nz>
2019-11-03 22:21:24 +1300

    @Ben trying to fix manual url handling

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/shop', require('./routes/shop'));
 
+/*
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+*/
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
   })
 }
 
